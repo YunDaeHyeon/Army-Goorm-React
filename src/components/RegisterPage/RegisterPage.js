c64eafe572e87924ccd2b36fae7b5d752163a327
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-04-29 15:13:27 +0000

    23.04.30 register page add

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from "react-router-dom";
+// react-hook-form 호출
+import { useForm } from 'react-hook-form';
 
 import "./RegisterPage_style.css";
 
 function RegisterPage() {
+  /*
+    watch : 해당 입력창에 나타나는 이벤트 확인
+    errors : 에러 수집
+  */
+  // useForm()의 내부 파라미터로 mode가 존재한다. ex) mode : "onChange"
+  const { register, watch, formState: {errors}} = useForm({mode : "onBlur"});
+
+  // name : password의 DOM 가져오기
+  const password = useRef();
+  // password의 필드 값(current)을 watch로 가져오기
+  password.current = watch("password");
+
+  // name : email의 input 관찰
+  // console.log(watch("email"));
 
   return (
     <div className='auth-wrapper'>
@@ -15,25 +31,65 @@ function RegisterPage() {
         <input
           name="email"
           type="email"
+          // 아무런 값이 입력되어있지 않거나, 이메일 정규식과 맞지 않으면 오류 반환
+          {...register("email", ({ required: true, pattern: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i }))}
         />
-
+        {
+          // 만약, errors 객체에 email의 이름을 가진 태그가 있다면 p태그 표시
+          errors.email && errors.email.type === "required" &&
+          <p>This field is required</p>
+        }
+        {
+          // 만약, errors 객체에 email의 이름을 가진 태그가 있다면 p태그 표시
+          errors.email && errors.email.type === "pattern" &&
+          <p>This is not a valid email format</p>
+        }
         <label>Name</label>
         <input
           name="name"
+          {...register("name", ({ required: true, maxLength : 10 }))}
         />
-
+        { // errors.name에 존재하는 에러 중 required 에러라면 해당 p 태그 반환
+          errors.name && errors.name.type === "required" &&
+          <p>This name field is required</p>
+        }
+        { // errors.name에 존재하는 에러 중 maxLength 에러라면 해당 p 태그 반환
+          errors.name && errors.name.type === "maxLength" &&
+          <p>Your input exceed maximum length</p>
+        }
         <label>Password</label>
         <input
-          name="Password"
+          name="password"
           type="password"
+          {...register("password", ({ required: true, minLength: 6 }))}
         />
-
+        {
+          errors.password && errors.password.type === "required" &&
+          <p>This password field is required</p>
+        }
+        {
+          errors.password && errors.password.type === "minLength" &&
+          <p>Password must have at least 6 characters</p>
+        }
         <label>Password Confirm</label>
         <input
           name="password_confirm"
           type="password"
+          {...register("password_confirm", ({
+            required: true, // name password(password.current)와 name password_confirm이 같으면.
+            // value : password_confirm의 내용
+            // password.current : useRef로 가져온 password의 내용
+            validate: (value) => value === password.current
+          }))}
         />
-        
+        {
+          errors.password_confirm && errors.password_confirm.type === "required" &&
+          <p>This password confirm field is required</p>
+        }
+        {
+          errors.password_confirm && errors.password_confirm.type === "validate" &&
+          <p>The passwords do not match</p>
+        }
         <input type='submit'/>
         <Link style={{color:'gray', textDecoration: 'none'}} to="/login">이미 아이디가 있다면...</Link>
       </form>
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import "firebase/auth"; // 파이어베이스 - 인증
 import "firebase/database"; // 파이어베이스 - 데이터베이스
 import "firebase/storage"; // 파이어베이스 - 스토리지
 
-var firebaseConfig = {
-    apiKey : "",
-    authDomain: "",
-    databaseURL : "",
-    projectId : "",
-    storageBucket : "",
-    messagingSenderId : "",
-    appId : "",
-    measurementId : "",
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1_AeEkJxuZsJb8xVebd7XfgZCpdQI7mo",
+  authDomain: "sw-study-army.firebaseapp.com",
+  projectId: "sw-study-army",
+  storageBucket: "sw-study-army.appspot.com",
+  messagingSenderId: "1028150656870",
+  appId: "1:1028150656870:web:f9f981b70cf4793360d2a7",
+  measurementId: "G-EPXNHCGVVM"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
