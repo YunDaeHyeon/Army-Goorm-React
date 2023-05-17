2aaa7c31c80fe98e8e0ae8b90ba2041f30addb0f
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-05-17 13:12:47 +0000

    feat: firebase와 메시지 패널 연동

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -3,9 +3,60 @@ import React, { Component } from 'react'
 import MessageHeader from './MessageHeader'
 import MessageForm from './MessageForm'
 import Message from './Message'
+import { connect } from 'react-redux';
+import firebase from '../../../firebase';
 
 export class MainPanel extends Component {
+
+  state = {
+    // firebase 접근 state
+    messagesRef: firebase.database().ref("messages"),
+    // 각 채팅방에 존재하는 메시지들 저장 용도
+    messages: [],
+    // 로딩
+    messagesLoading: true,
+  }
+
+  componentDidMount(){ // 컴포넌트 마운트 시
+    const { chatRoom } = this.props; // props(state)에 존재하는 chatRoom 가져오기
+
+    if(chatRoom){ // chatRoom이 존재하면 (존재하지 않다는 것은 에러.)
+    // 매개변수는 클릭한 채팅방의 id를 가져와 이를 토대로 메시지 호출
+    this.addMessagesListeners(chatRoom.id);
+    }
+  }
+
+  addMessagesListeners = (chatRoomId) => {
+    let messagesArray = [];
+    this.state.messagesRef
+      .child(chatRoomId)
+      // child_added 이벤트 리스너 실행 (실시간으로 데이터 가져오기)
+      .on("child_added", DataSnapshot => {
+        messagesArray.push(DataSnapshot.val());
+        this.setState({ 
+          // 실시간으로 불러온 데이터(messageArray)를 messages(state)로 이동
+          messages: messagesArray,
+          // 최초 데이터 불러올 때는 로딩중, (true) 완료 시 false
+          messagesLoading : false,
+        });
+      })
+  }
+
+  renderMessages = (messages) => {
+    messages.length > 0 && // messages가 하나 이상의 메시지를 가지고 있다면
+    // Message Component 사용
+    messages.map(message => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.props.user}
+      />
+    ))
+  }
+
   render() {
+    // render()가 실행될때마다 state에서 messages 가져오기
+    const { messages } = this.state;
     return (
         <div style={{ padding: '2rem 2rem 0 2rem'}}>
             
@@ -21,7 +72,7 @@ export class MainPanel extends Component {
                 overflowY: 'auto'
             }}>
             </div>
-
+              {this.renderMessages(messages)}
             <MessageForm/>
 
         </div>
@@ -29,4 +80,12 @@ export class MainPanel extends Component {
   }
 }
 
-export default MainPanel
+// redux에 있는 state를 prop으로 호출
+const mapStateToProps = state => {
+  return{
+    user: state.user.currentUser,
+    chatRoom: state.chatRoom.currentChatRoom
+  }
+}
+
+export default connect(mapStateToProps)(MainPanel);
