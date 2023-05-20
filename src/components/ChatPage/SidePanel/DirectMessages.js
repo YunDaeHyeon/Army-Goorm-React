c80c80e0477ed1911c9b160d191dd9d648c64920
Daehyeon Yun <62231651+YunDaeHyeon@users.noreply.github.com>
2023-05-20 09:28:50 +0000

    feat: 다이렉트 메시지 패널 UI 추가

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+// react-icons
+import { FaRegSmile } from 'react-icons/fa';
 
-function DirectMessages() {
-  return (
-    <div>DirectMessages</div>
-  )
+export class DirectMessages extends Component {
+
+  renderDirectMessages = () => {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <span style={{ display:'flex', alignItems:'center' }}>
+          <FaRegSmile style={{ marginRight: 3 }}/> DIRECT MESSAGES(1)
+        </span>
+
+        <ul style={{ listStyleType: 'none', padding: 0}}>
+          {this.renderDirectMessages()}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default DirectMessages
